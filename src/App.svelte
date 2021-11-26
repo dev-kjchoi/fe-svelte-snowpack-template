e492f67c5ef98ed96f8efc89c8dce524859d1041
dev-kjchoi <kwangjiya@gmail.com>
2021-11-26 17:36:10 +0900

    create project

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { todos } from "~/store";
+  import CreateTodo from "~/components/CreateTodo";
+</script>
+
+<div class="container">
+  <CreateTodo />
+  {#each $todos as todo (todo.id)}
+    <div>{todo.title}</div>
+  {/each}
+</div>
+
+<style lang="scss"></style>
